ede566942444e870a1dcfeafefa0588cffc2918b
pmodv <pvernes@gmail.com>
2023-07-19 16:48:15 -0400

    update

--- bin/main.jl
+++ bin/main.jl
@@ -1,29 +1,6 @@
 using Oxygen,HTTP
 #using OxyServer 
 
-
-# initialize default model instance with value 3000
-# Model struct defined in OxyServer.jl
-model = Model(3000)
-
-
-# mutable because we will update the field value of an existing struct in this server
-mutable struct Model
-    x::Int32 
-end
-
-# Dummy simulation function 
-function simulate(a::Int32)
-    model = Model(a)
-    println("Simulation Complete for model with parameter ",model.x)
-end
-
-
-
-function bootServer()
-    serve(host="0.0.0.0")
-end
-
 function setValue(m::Model,a::Int32)
     # mutate existing model
     m.x = a
--- src/OxyServer.jl
+++ src/OxyServer.jl
@@ -1,7 +1,30 @@
 module OxyServer
-#=
+
 using Oxygen
 export Model, bootServer, simulate
-=#
+
+
+
+# initialize default model instance with value 3000
+# Model struct defined in OxyServer.jl
+model = Model(3000)
+
+
+# mutable because we will update the field value of an existing struct in this server
+mutable struct Model
+    x::Int32 
+end
+
+# Dummy simulation function 
+function simulate(a::Int32)
+    model = Model(a)
+    println("Simulation Complete for model with parameter ",model.x)
+end
+
+
+
+function bootServer()
+    serve(host="0.0.0.0")
+end
 
 end # end module
