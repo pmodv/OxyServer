b140bd7063a501421717ca4a98007bec58bc6ae1
pmodv <pvernes@gmail.com>
2023-07-19 16:11:01 -0400

    fix

--- bin/main.jl
+++ bin/main.jl
@@ -1,37 +1,11 @@
 using Oxygen,HTTP
-# using OxyServer for when exporting Symbols (functions, structs) for OxyServer.jl
+using OxyServer 
 
 
-# mutable because we will update the field value of an existing struct in this server
-mutable struct Model
-    x::Int32 
-end
+# initialize default model instance with value 3000
+model = Model(3000)
 
-# Dummy simulation function
-function simulate(a::Int32)
-    model = Model(a)
-    println("Simulation Complete for model with parameter ",model.x)
-end
 
-function setValue(m::Model,a::Int32)
-    # mutate existing model
-    m.x = a
-    return m.x
-end
-
-function getValue() 
-    return model.x
-end
-
-function getModel()
-    return model
-end
-
-function bootServer()
-    # initialize default model instance with value 3000
-    model = Model(3000)
-    serve(host="0.0.0.0")
-end
 
 
 # "Interpolate" variable x in the endpoint
--- src/OxyServer.jl
+++ src/OxyServer.jl
@@ -1,4 +1,33 @@
 module OxyServer
+export Model, simulate, setValue, getValue, getModel, bootServer
+# mutable because we will update the field value of an existing struct in this server
+mutable struct Model
+    x::Int32 
+end
 
+
+# Dummy simulation function
+function simulate(a::Int32)
+    model = Model(a)
+    println("Simulation Complete for model with parameter ",model.x)
+end
+
+function setValue(m::Model,a::Int32)
+    # mutate existing model
+    m.x = a
+    return m.x
+end
+
+function getValue() 
+    return model.x
+end
+
+function getModel()
+    return model
+end
+
+function bootServer()
+    serve(host="0.0.0.0")
+end
 
 end # end module
