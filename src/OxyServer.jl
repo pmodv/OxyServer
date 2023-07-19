af411551d17e869aa487b751d47b3e38f5ecb7a9
pmodv <pvernes@gmail.com>
2023-07-19 16:19:10 -0400

    dep

--- src/OxyServer.jl
+++ src/OxyServer.jl
@@ -1,4 +1,6 @@
 module OxyServer
+
+using Oxygen
 export Model, simulate, setValue, getValue, getModel, bootServer
 # mutable because we will update the field value of an existing struct in this server
 mutable struct Model
