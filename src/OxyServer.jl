eb47d2c3313cd88746f4fed14ddf795c91005bf7
pmodv <pvernes@gmail.com>
2023-07-19 16:42:05 -0400

    fix

--- src/OxyServer.jl
+++ src/OxyServer.jl
@@ -1,24 +1,7 @@
 module OxyServer
-
+#=
 using Oxygen
 export Model, bootServer, simulate
-
-
-# mutable because we will update the field value of an existing struct in this server
-mutable struct Model
-    x::Int32 
-end
-
-# Dummy simulation function
-function simulate(a::Int32)
-    model = Model(a)
-    println("Simulation Complete for model with parameter ",model.x)
-end
-
-
-
-function bootServer()
-    serve(host="0.0.0.0")
-end
+=#
 
 end # end module
